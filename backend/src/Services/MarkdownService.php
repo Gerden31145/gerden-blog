f399e95d6f85a5ea4476094c55de16e45d2661e8
Gerden31145 <327566107@qq.com>
2026-06-09 14:28:23 +0800

    fixed:修复toc生成的bug

--- backend/src/Services/MarkdownService.php
+++ backend/src/Services/MarkdownService.php
@@ -6,61 +6,117 @@ class MarkdownService
 {
   public static function render(string $markdown): array
   {
-    $toc = self::extractToc($markdown);
-
     $parsedown = new \Parsedown();
     $parsedown->setSafeMode(true);
 
     $html = $parsedown->text($markdown);
-    $html = self::injectHeadingIds($html, $toc);
 
-    return [
-      'html' => $html,
-      'toc' => $toc
-    ];
+    return self::injectHeadingIdsAndExtractToc($html);
   }
 
-  private static function extractToc(string $markdown): array
+  private static function injectHeadingIdsAndExtractToc(string $html): array
   {
-    preg_match_all('/^(#{1,6})\s+(.+)$/m', $markdown, $matches, PREG_SET_ORDER);
+    if (!class_exists(\DOMDocument::class)) {
+      throw new \RuntimeException('PHP DOM extension is not enabled');
+    }
+
+    $dom = new \DOMDocument('1.0', 'UTF-8');
+    $previousErrorState = libxml_use_internal_errors(true);
+
+    $dom->loadHTML(
+      '<?xml encoding="UTF-8"><div id="markdown-root">' . $html . '</div>',
+      LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD
+    );
+
+    libxml_clear_errors();
+    libxml_use_internal_errors($previousErrorState);
+
+    $root = self::findMarkdownRoot($dom);
+
+    if (!$root) {
+      return [
+        'html' => $html,
+        'toc' => []
+      ];
+    }
+
+    $xpath = new \DOMXPath($dom);
+    $headings = $xpath->query(
+      './/*[self::h1 or self::h2 or self::h3 or self::h4 or self::h5 or self::h6]',
+      $root
+    );
 
     $toc = [];
     $slugCount = [];
 
-    foreach ($matches as $match) {
-      $depth = strlen($match[1]);
-      $text = trim($match[2]);
+    foreach ($headings as $heading) {
+      if (!$heading instanceof \DOMElement) {
+        continue;
+      }
 
-      $baseSlug = strtolower(trim(preg_replace('/\s+/', '-', $text)));
-      $slugCount[$baseSlug] = ($slugCount[$baseSlug] ?? 0) + 1;
+      $text = trim(preg_replace('/\s+/u', ' ', $heading->textContent));
 
-      $id = $slugCount[$baseSlug] > 1
-        ? $baseSlug . '-' . $slugCount[$baseSlug]
-        : $baseSlug;
+      if ($text === '') {
+        continue;
+      }
+
+      $id = self::generateHeadingId($text, $slugCount);
+      $heading->setAttribute('id', $id);
 
       $toc[] = [
         'id' => $id,
         'text' => $text,
-        'depth' => $depth
+        'depth' => (int) substr($heading->tagName, 1)
       ];
     }
 
-    return $toc;
+    return [
+      'html' => self::innerHtml($dom, $root),
+      'toc' => $toc
+    ];
   }
 
-  private static function injectHeadingIds(string $html, array $toc): string
+  private static function findMarkdownRoot(\DOMDocument $dom): ?\DOMElement
   {
-    $index = 0;
-
-    return preg_replace_callback('/<h([1-6])>(.*?)<\/h\1>/s', function ($matches) use (&$index, $toc) {
-      if (!isset($toc[$index])) {
-        return $matches[0];
+    foreach ($dom->getElementsByTagName('div') as $node) {
+      if ($node instanceof \DOMElement && $node->getAttribute('id') === 'markdown-root') {
+        return $node;
       }
+    }
 
-      $id = htmlspecialchars($toc[$index]['id'], ENT_QUOTES, 'UTF-8');
-      $index++;
+    return null;
+  }
 
-      return "<h{$matches[1]} id=\"{$id}\">{$matches[2]}</h{$matches[1]}>";
-    }, $html);
+  private static function generateHeadingId(string $text, array &$slugCount): string
+  {
+    $baseSlug = self::slugify($text);
+    $slugCount[$baseSlug] = ($slugCount[$baseSlug] ?? 0) + 1;
+
+    return $slugCount[$baseSlug] > 1
+      ? $baseSlug . '-' . $slugCount[$baseSlug]
+      : $baseSlug;
+  }
+
+  private static function slugify(string $text): string
+  {
+    $text = function_exists('mb_strtolower')
+      ? mb_strtolower($text, 'UTF-8')
+      : strtolower($text);
+
+    $slug = preg_replace('/[^\p{L}\p{N}]+/u', '-', $text);
+    $slug = trim($slug, '-');
+
+    return $slug !== '' ? $slug : 'section';
+  }
+
+  private static function innerHtml(\DOMDocument $dom, \DOMElement $element): string
+  {
+    $html = '';
+
+    foreach ($element->childNodes as $child) {
+      $html .= $dom->saveHTML($child);
+    }
+
+    return $html;
   }
 }
