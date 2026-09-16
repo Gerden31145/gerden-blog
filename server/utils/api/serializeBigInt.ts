334e3f92d7657d047bb99eb8855d6662b7794a0d
Gerden31145 <327566107@qq.com>
2026-09-16 11:05:48 +0800

    chore: remove legacy Nuxt backend and clean up dependencies

--- server/utils/api/serializeBigInt.ts
+++
@@ -1,6 +0,0 @@
-// 将BigInt类型转为字符串
-export default function serializeBigInt<T, U>(data: T): U {
-  return JSON.parse(JSON.stringify(data, (_, value) => {
-    return typeof value === 'bigint' ? value.toString() : value
-  }))
-} 
